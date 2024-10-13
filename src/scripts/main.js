f12410539a78f66875e8bdbcb1ceff09918db9b5
Pavlo <oksanakopalko@gmail.com>
2024-10-13 22:52:09 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,21 +1,25 @@
 'use strict';
 
-const allLi = document.querySelectorAll('li');
+const allLi = document.querySelector('.tree').querySelectorAll('li');
 const tree = document.querySelector('.tree');
 
 for (const li of allLi) {
   if (li.children.length !== 0) {
-    const span = document.createElement('span');
+    // <li> those haven't elements inside must be ignored
+    const span = document.createElement('span'); // we need just li those have.
 
-    li.prepend(span);
-    span.append(span.nextSibling);
+    // Create element span and insert it in every <li> we get in this condition
+    li.prepend(span); // span tag has a text like a 'neighbor' sibling
+    span.append(span.nextSibling); // move text inside in span
   }
 }
 
 tree.addEventListener('click', function (e) {
   if (e.target.tagName !== 'SPAN') {
-    return;
-  }
+    // we want event work
+    return; // just at the span tag, none anythink else
+  } // we want to hide another 'li' those aren't the title
 
   e.target.nextSibling.hidden = !e.target.nextSibling.hidden;
+  // we exchange meaning of properties 'hidden' on true at false and otherwise
 });
