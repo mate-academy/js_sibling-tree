05bff91318544d80961cf5172c017eaf26bf63e7
Pavlo <oksanakopalko@gmail.com>
2024-10-13 21:23:38 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,21 @@
 'use strict';
 
-// write code here
+const allLi = document.querySelectorAll('li');
+const tree = document.querySelector('.tree');
+
+for (const li of allLi) {
+  if (li.children.length !== 0) {
+    const span = document.createElement('span');
+
+    li.prepend(span);
+    span.append(span.nextSibling);
+  }
+}
+
+tree.addEventListener('click', function (e) {
+  if (e.target.tagName !== 'SPAN') {
+    return;
+  }
+
+  e.target.nextSibling.hidden = !e.target.nextSibling.hidden;
+});
