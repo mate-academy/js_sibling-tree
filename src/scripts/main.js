04d290a77f842d1a9d0ceb531cf1758fa8d4f1c0
Max <gcrazy007@gmail.com>
2024-10-20 09:46:33 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,25 @@
 'use strict';
 
-// write code here
+const tree = document.querySelector('.tree');
+const liArray = [...document.querySelectorAll('li')];
+const liEl = liArray.filter((li) => li.querySelector('ul'));
+
+liEl.forEach((li) => {
+  const span = document.createElement('span');
+
+  span.textContent = li.firstChild.textContent;
+  li.firstChild.remove();
+  li.prepend(span);
+});
+
+tree.addEventListener('click', (e) => {
+  if (e.target.tagName === 'SPAN') {
+    const parentLi = e.target.parentElement;
+    const subList = parentLi.querySelector('ul');
+
+    if (subList) {
+      subList.style.display =
+        subList.style.display === 'none' ? 'block' : 'none';
+    }
+  }
+});
