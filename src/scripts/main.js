15882ee57ddfdb000422a5fe818ed12db7345742
Bronex25 <oleksandr.plaksin25@gmail.com>
2024-10-21 13:21:25 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,27 @@
 'use strict';
 
-// write code here
+const tree = document.querySelector('.tree');
+const headers = tree.querySelectorAll('li');
+
+headers.forEach((head) => {
+  const check = head.querySelector('ul');
+
+  if (check) {
+    const spanElem = document.createElement('span');
+
+    spanElem.textContent = head.firstChild.textContent;
+    head.firstChild.replaceWith(spanElem);
+  }
+});
+
+tree.querySelectorAll('span').forEach((elem) => {
+  elem.addEventListener('click', (e) => {
+    if (e.currentTarget.nextElementSibling.hidden) {
+      e.currentTarget.nextElementSibling.hidden = false;
+    } else {
+      e.currentTarget.nextElementSibling.hidden = true;
+    }
+
+    e.stopPropagation();
+  });
+});
