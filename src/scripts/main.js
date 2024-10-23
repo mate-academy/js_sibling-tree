78c0295cb1b7c0ac8d3dcafa1c68180a47770197
Vasyl Pylypchynets <pylypchynets.vasyl@gmail.com>
2024-10-23 17:21:01 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,38 @@
 'use strict';
 
 // write code here
+
+const tree = document.querySelector('.tree');
+
+function wrapTextInSpans() {
+  const listItems = tree.querySelectorAll('li');
+
+  listItems.forEach((li) => {
+    if (li.firstChild.nodeType === Node.TEXT_NODE && li.querySelector('ul')) {
+      const span = document.createElement('span');
+
+      span.textContent = li.firstChild.textContent.trim();
+      li.firstChild.replaceWith(span);
+    }
+  });
+}
+
+wrapTextInSpans();
+
+tree.addEventListener('click', (e) => {
+  const clickedSpan = e.target.closest('span');
+
+  if (!clickedSpan) {
+    return;
+  }
+
+  const li = clickedSpan.parentElement;
+
+  const nestedList = li.querySelector('ul');
+
+  if (nestedList) {
+    const isHidden = nestedList.style.display === 'none';
+
+    nestedList.style.display = isHidden ? '' : 'none';
+  }
+});
