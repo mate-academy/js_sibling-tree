fdc0d31dc7c98a5e22ecf6dffb97ad6a037383ec
Ivan Paiareli <ipayareli@gmail.com>
2024-10-30 20:14:31 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,42 @@
 'use strict';
 
-// write code here
+const headings = document.querySelectorAll('li');
+
+const headingsTexts = [
+  'Fruit',
+  'Fish',
+  'Red',
+  'Yellow',
+  'Ocean fishes',
+  'Freshwater fish',
+];
+
+headings.forEach((item) => {
+  if (headingsTexts.includes(item.childNodes[0].textContent.trim())) {
+    item.classList.add('heading');
+  }
+});
+
+const style = document.createElement('style');
+
+document.head.appendChild(style);
+
+style.textContent = `
+  .classToHide > ul {
+    display: none;
+  }
+`;
+
+const headingsWithClass = document.getElementsByClassName('heading');
+
+Array.from(headingsWithClass).forEach((item) => {
+  item.style.cursor = 'pointer';
+
+  item.addEventListener('click', (e) => {
+    e.stopPropagation();
+
+    if (e.target.closest('.heading')) {
+      item.classList.toggle('classToHide');
+    }
+  });
+});
