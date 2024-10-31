461614df759ad0450cbf05b9bee8bba72429900f
Alina Luchytska <aluchitska@gmail.com>
2024-10-31 22:14:07 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,24 @@
 'use strict';
 
 // write code here
+const listOfLi = document.querySelectorAll('li');
+
+for (const li of listOfLi) {
+  const product = li.firstChild;
+  const span = document.createElement('span');
+
+  span.textContent = product.textContent.trim();
+  li.replaceChild(span, product);
+
+  span.onclick = function () {
+    const ul = li.querySelector('ul');
+
+    if (ul) {
+      if (ul.style.display === 'none') {
+        ul.style.display = 'block';
+      } else {
+        ul.style.display = 'none';
+      }
+    }
+  };
+}
