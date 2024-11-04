55ad0e7013c684062b07f79efa0d23237681f3f9
Stanislav Kohut <stako211@gmail.com>
2024-11-04 20:22:23 +0200

    added task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,26 @@
 'use strict';
 
-// write code here
+const listItems = document.querySelectorAll('.tree li');
+
+listItems.forEach((item) => {
+  if (item.querySelector('ul')) {
+    const span = document.createElement('span');
+    const originalText = item.firstChild.textContent.trim();
+
+    span.textContent = originalText;
+    item.firstChild.textContent = '';
+    item.insertBefore(span, item.firstChild);
+  }
+});
+
+const titles = document.querySelectorAll('span');
+
+titles.forEach((title) => {
+  title.addEventListener('click', () => {
+    if (title.nextElementSibling.style.display === 'none') {
+      title.nextElementSibling.style.display = '';
+    } else {
+      title.nextElementSibling.style.display = 'none';
+    }
+  });
+});
