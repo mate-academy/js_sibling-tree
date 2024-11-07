48e2c9edf9b19c9d33658dbb5c5a85a445a62225
Karina Koliada <koladakarina1112@gmail.com>
2024-11-07 20:14:20 +0100

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,20 @@
 'use strict';
 
-// write code here
+'use strict';
+
+document.querySelectorAll('.tree > li').forEach((item) => {
+  const span = document.createElement('span');
+
+  span.textContent = item.firstChild.textContent.trim();
+
+  item.firstChild.replaceWith(span);
+
+  span.addEventListener('click', () => {
+    const subList = item.querySelector('ul');
+
+    if (subList) {
+      subList.style.display =
+        subList.style.display === 'none' ? 'block' : 'none';
+    }
+  });
+});
