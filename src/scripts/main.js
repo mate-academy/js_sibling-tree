53360705e77233854b8b6c32d19404b8f29b9b57
Artur Misko <arthurmisko@gmail.com>
2024-11-11 14:57:34 +0100

    done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,30 @@
 'use strict';
 
-// write code here
+const tree = document.querySelector('.tree');
+
+function makeTreeExpandable(root) {
+  root.querySelectorAll('li').forEach((li) => {
+    if (li.querySelector('ul')) {
+      const span = document.createElement('span');
+
+      span.textContent = li.childNodes[0].textContent;
+      li.childNodes[0].textContent = '';
+      li.insertBefore(span, li.childNodes[0]);
+
+      span.addEventListener('click', () => {
+        li.querySelector('ul').classList.toggle('hidden');
+      });
+    }
+  });
+}
+
+const style = document.createElement('style');
+
+style.textContent = `
+  .hidden {
+    display: none;
+  }
+`;
+document.head.appendChild(style);
+
+makeTreeExpandable(tree);
