91b412e70e6a38c3684642d61f8233f676dd8e3e
Mykola <mykola.stanj@gmail.com>
2024-11-14 14:18:58 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,21 @@
 'use strict';
 
 // write code here
+document.querySelectorAll('.tree li').forEach((li) => {
+  const span = document.createElement('span');
+
+  span.textContent = li.firstChild.textContent.trim();
+
+  li.replaceChild(span, li.firstChild);
+});
+
+document.querySelectorAll('.tree li > span').forEach((span) => {
+  span.addEventListener('click', () => {
+    const nestedUl = span.parentElement.querySelector('ul');
+
+    if (nestedUl) {
+      nestedUl.style.display =
+        nestedUl.style.display === 'none' ? 'block' : 'none';
+    }
+  });
+});
