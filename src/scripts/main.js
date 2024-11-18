fb9cb10a0fe55bf928b0cb4413838a6849426aa8
Yevhenii Kushnir <aroblack17@gmail.com>
2024-11-18 21:51:55 +0100

    Sliding tree

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,35 @@
 'use strict';
 
 // write code here
+const tree = document.querySelector('.tree');
+
+(function wrapHeaderTextInSpan() {
+  const listOfLi = [...document.querySelectorAll('li')];
+
+  listOfLi.forEach((li) => {
+    const textNode = li.firstChild;
+
+    if (
+      textNode &&
+      textNode.nodeType === Node.TEXT_NODE &&
+      li.querySelector('ul')
+    ) {
+      const span = document.createElement('span');
+
+      span.textContent = textNode.textContent.trim();
+      textNode.replaceWith(span);
+    }
+  });
+})();
+
+tree.addEventListener('click', (e) => {
+  const spanElement = e.target.closest('span');
+
+  if (spanElement) {
+    const ul = spanElement.parentElement.querySelector('ul');
+
+    if (ul) {
+      ul.hidden = !ul.hidden;
+    }
+  }
+});
