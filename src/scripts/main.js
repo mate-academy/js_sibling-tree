0209f1f54f823a87cab7b94aca2190c7f4ae65ab
Oleksandr Rad <clavigo365@gmail.com>
2024-11-19 15:40:53 +0200

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,28 @@
 'use strict';
 
 // write code here
+const listItems = document.querySelectorAll('li');
+
+listItems.forEach((item) => {
+  if (item.querySelector('ul')) {
+    const header = item.firstChild;
+
+    if (header.nodeType === 3) {
+      const pTag = document.createElement('span');
+
+      pTag.textContent = header.textContent;
+      item.insertBefore(pTag, item.firstChild);
+      item.removeChild(header);
+
+      pTag.addEventListener('click', (e) => {
+        const nextUl = pTag.nextElementSibling;
+
+        if (nextUl.style.display === 'none') {
+          nextUl.style.display = 'block';
+        } else {
+          nextUl.style.display = 'none';
+        }
+      });
+    }
+  }
+});
