3accf8828c6928bbf6d4a230f58a514d6eda1296
Dominik Dudek <dom-dud774@o2.pl>
2024-11-19 22:20:30 +0100

    Task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,38 @@
 'use strict';
 
 // write code here
+
+const list = document.querySelector('.tree');
+
+function wrapHeaders(subList) {
+  if (!subList) {
+    return;
+  }
+
+  [...subList.children].forEach((item) => {
+    const innerList = item.querySelector('ul');
+
+    if (innerList && item.firstChild.nodeType === Node.TEXT_NODE) {
+      const span = document.createElement('span');
+
+      span.textContent = item.firstChild.textContent;
+
+      item.replaceChild(span, item.firstChild);
+    }
+
+    if (innerList) {
+      wrapHeaders(innerList);
+    }
+  });
+}
+
+wrapHeaders(list);
+
+list.addEventListener('click', (e) => {
+  if (e.target.tagName === 'SPAN') {
+    const nestedList = e.target.nextElementSibling;
+
+    nestedList.style.display =
+      nestedList.style.display === 'none' ? 'block' : 'none';
+  }
+});
