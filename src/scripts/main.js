304b41fe457ab16e5adcf17848a921530a16e4a9
OkMoroz <k.moroz.mail@gmail.com>
2024-11-22 00:23:20 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,22 @@
 'use strict';
 
-// write code here
+const treeItems = document.querySelectorAll('.tree li');
+
+treeItems.forEach((item) => {
+  const sublist = item.querySelector('ul');
+
+  if (sublist) {
+    const header = document.createElement('span');
+    const parentElement = sublist.parentElement;
+
+    header.textContent = parentElement.firstChild.textContent.trim();
+
+    item.firstChild.replaceWith(header);
+
+    header.addEventListener('click', () => {
+      const isVisible = sublist.style.display !== 'none';
+
+      sublist.style.display = isVisible ? 'none' : 'block';
+    });
+  }
+});
