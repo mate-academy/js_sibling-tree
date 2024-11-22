747e291c894181d7ebc483f05806f33c50edea47
LirikTop <melnicukkiril2002@gmail.com>
2024-11-22 20:15:56 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,29 @@
 'use strict';
 
-// write code here
+const tree = document.querySelector('.tree');
+const ul = [...tree.querySelectorAll('ul')];
+
+ul.forEach((elem) => {
+  const header = elem.parentNode.firstChild;
+  const span = document.createElement('span');
+
+  span.textContent = header.textContent;
+  header.remove();
+  elem.parentNode.insertBefore(span, elem);
+});
+
+// eslint-disable-next-line no-shadow
+tree.addEventListener('click', (event) => {
+  const span = event.target.closest('span');
+
+  if (span) {
+    const list = span.nextElementSibling;
+    const isOpen = list.classList.toggle('open');
+
+    if (isOpen) {
+      list.style.display = 'none';
+    } else {
+      list.style.display = 'block';
+    }
+  }
+});
