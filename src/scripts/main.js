6987f5375ce0570d3fae902724e948c9281383cd
Denys Kovalchuk <denkov414@gmail.com>
2024-11-27 16:46:42 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,28 @@
 'use strict';
 
-// write code here
+document.addEventListener('DOMContentLoaded', () => {
+  const tree = document.querySelector('.tree');
+
+  tree.querySelectorAll('li').forEach((li) => {
+    const textNode = li.childNodes[0];
+
+    if (textNode.nodeType === Node.TEXT_NODE) {
+      const span = document.createElement('span');
+
+      span.textContent = textNode.textContent.trim();
+      li.insertBefore(span, textNode);
+      li.removeChild(textNode);
+    }
+  });
+
+  tree.addEventListener('click', (e) => {
+    if (e.target.tagName === 'SPAN') {
+      const parentLi = e.target.parentElement;
+      const subTree = parentLi.querySelector('ul');
+
+      if (subTree) {
+        subTree.hidden = !subTree.hidden;
+      }
+    }
+  });
+});
