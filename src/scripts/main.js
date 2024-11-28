5986ff5287e4b5b2d1047f981b71dbb47a14e59d
marichkamt <emilycry1999@gmail.com>
2024-11-28 15:09:46 +0100

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,27 @@
 'use strict';
 
-// write code here
+const tree = document.querySelector('.tree');
+const LiElement = tree.querySelectorAll('li');
+
+LiElement.forEach((listItem) => {
+  const ulElement = listItem.querySelector('ul');
+
+  if (ulElement) {
+    const span = document.createElement('span');
+    const text = listItem.firstChild;
+
+    span.textContent = text.textContent;
+    listItem.insertBefore(span, text);
+    text.remove();
+  }
+});
+
+tree.addEventListener('click', (e) => {
+  if (e.target.tagName === 'SPAN') {
+    const nextSibling = e.target.nextSibling;
+
+    if (nextSibling && nextSibling.tagName === 'UL') {
+      nextSibling.hidden = !nextSibling.hidden;
+    }
+  }
+});
