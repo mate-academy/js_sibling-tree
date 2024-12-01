d4741d873ae0ef1c809b4005ca1ffe03bc007217
Oleksandr <tokyopac@gmail.com>
2024-12-01 19:32:36 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,30 @@
 'use strict';
 
-// write code here
+const tree = document.querySelector('.tree');
+
+if (!tree) {
+  throw new Error('List not defined');
+}
+
+for (const li of tree.querySelectorAll('li')) {
+  const span = document.createElement('span');
+  const textNode = li.firstChild;
+
+  span.append(textNode);
+  li.prepend(span);
+}
+
+tree.onclick = (e) => {
+  if (e.target.tagName !== 'SPAN') {
+    return false;
+  }
+  e.stopPropagation();
+
+  const childrenContainer = e.target.parentNode.querySelector('ul');
+
+  if (!childrenContainer) {
+    return false;
+  }
+
+  childrenContainer.hidden = !childrenContainer.hidden;
+};
