b6617961ad3c6be5af9dcaa626e7b2ece553e79f
Александр Ляшенко <sanko.lialko14@gmail.com>
2024-12-12 00:40:31 +0200

    done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,23 @@
 'use strict';
 
 // write code here
+
+const items = document.querySelector('li');
+const tree = document.querySelector('.tree');
+
+for (const item of items) {
+  const span = document.querySelector('span');
+
+  item.prepend(span);
+  span.append(span.nextSibling);
+
+  tree.onclick = (e) => {
+    if (e.target.tagName !== 'SPAN') {
+      return;
+    }
+
+    const childrenContainer = e.target.parentNode.querySelector('ul');
+
+    childrenContainer.hidden = !childrenContainer.hidden;
+  };
+}
