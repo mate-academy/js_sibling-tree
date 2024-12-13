9ee0a5089c953a5b7381de757058eab2d805b66d
Yaroslav <nest7.an@gmail.com>
2024-12-14 00:57:41 +0200

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,35 @@
 'use strict';
 
-// write code here
+const tree = document.querySelector('.tree');
+
+const listItems = [...tree.querySelectorAll('li')];
+
+listItems.forEach((elem) => {
+  const firstText = elem.firstChild;
+  const span = document.createElement('span');
+
+  span.textContent = `${firstText.textContent}`;
+
+  elem.replaceChild(span, firstText);
+});
+
+// eslint-disable-next-line no-shadow
+function clickOnSpan(event) {
+  const spanTarget = event.target.closest('span');
+
+  if (spanTarget) {
+    const list = spanTarget.nextElementSibling;
+
+    const computedStyles = getComputedStyle(list);
+
+    if (computedStyles.display === 'none') {
+      list.style.display = 'block';
+
+      return;
+    }
+
+    list.style.display = 'none';
+  }
+}
+
+tree.addEventListener('click', clickOnSpan);
