3138e9fb2e3767e2b4b2aebb121cdc2223b21d18
Oleh Lukash <lukasholeg02@gmail.com>
2024-12-15 01:59:53 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,37 @@
 'use strict';
 
-// write code here
+const tree = document.querySelector('.tree');
+
+function expandTree(treeIn) {
+  for (const item of treeIn.querySelectorAll('li')) {
+    if (item.querySelector('ul')) {
+      expandTree(item.querySelector('ul'));
+
+      const newSpan = document.createElement('span');
+
+      newSpan.textContent = item.firstChild.textContent.trim();
+
+      item.firstChild.remove();
+      item.insertBefore(newSpan, item.querySelector('ul'));
+    }
+  }
+}
+
+expandTree(tree);
+
+document.addEventListener('click', (e) => {
+  if (e.target.tagName === 'SPAN') {
+    const childList = e.target.nextElementSibling;
+
+    if (childList && childList.tagName === 'UL') {
+      if (
+        childList.style.display === '' ||
+        childList.style.display === 'block'
+      ) {
+        childList.style.display = 'none';
+      } else {
+        childList.style.display = 'block';
+      }
+    }
+  }
+});
