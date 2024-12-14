0fe74bd698cf204620916dca4cde1a3ed644c049
Vlad <vladyslav.vasylevych@gmail.com>
2024-12-15 00:10:47 +0200

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,27 @@
 'use strict';
 
 // write code here
+
+const lis = document.querySelectorAll('li');
+
+for (const li of lis) {
+  const span = document.createElement('span');
+
+  span.addEventListener('click', (e) => {
+    const elem = e.currentTarget.nextSibling;
+
+    if (elem === null) {
+      return;
+    }
+
+    if (elem.style.display === 'none') {
+      elem.style.display = 'block';
+    } else {
+      elem.style.display = 'none';
+    }
+  });
+
+  span.appendChild(document.createTextNode(li.firstChild.nodeValue));
+
+  li.replaceChild(span, li.firstChild);
+}
