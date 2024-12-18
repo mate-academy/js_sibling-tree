74ff55b40a35e05bbe4dfaea50d73d44689b4979
Iryna <irakogut23190@gmail.com>
2024-12-18 19:28:50 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,28 @@
 'use strict';
 
-// write code here
+const li = document.querySelectorAll('li');
+
+const liArray = Array.from(li);
+const names = [];
+
+for (const nam of liArray) {
+  const textNode = nam.firstChild;
+
+  const span = document.createElement('span');
+
+  span.textContent = textNode.textContent;
+
+  nam.replaceChild(span, textNode);
+
+  names.push(span);
+}
+
+names.forEach((n) => {
+  n.addEventListener('click', (e) => {
+    if (n.parentNode.querySelector('ul')) {
+      const ul = n.parentNode.querySelector('ul');
+
+      ul.style.display = 'none';
+    }
+  });
+});
