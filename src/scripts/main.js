2ba3c565c8b2f42eeb4ceb5e04ace66838783604
Vladyslav Kriuk <kriukvlad@gmail.com>
2024-12-20 23:08:17 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,24 @@
 'use strict';
 
 // write code here
+const li = document.querySelectorAll('li');
+
+li.forEach((item) => {
+  const span = document.createElement('span');
+
+  span.textContent = item.firstChild.textContent.trim();
+  item.firstChild.textContent = '';
+  item.prepend(span);
+
+  span.addEventListener('click', () => {
+    const childUl = item.querySelector('ul');
+
+    if (childUl) {
+      if (childUl.style.display === 'none') {
+        childUl.style.display = '';
+      } else {
+        childUl.style.display = 'none';
+      }
+    }
+  });
+});
