366bbee81425833a2e30b42222da8cd69a924a07
Maksym Korchynskyi <maksym.24.09.2003@gmail.com>
2024-12-23 11:38:11 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,23 @@
 'use strict';
 
-// write code here
+function createSlidingTree(tree) {
+  [...tree.children].forEach((branch) => {
+    const innerTree = branch.querySelector('ul');
+
+    if (innerTree !== null) {
+      createSlidingTree(innerTree);
+
+      const title = document.createElement('span');
+
+      title.textContent = branch.firstChild.textContent;
+      branch.firstChild.replaceWith(title);
+
+      title.addEventListener('click', () => {
+        innerTree.style.display =
+          innerTree.style.display === 'none' ? '' : 'none';
+      });
+    }
+  });
+}
+
+createSlidingTree(document.querySelector('.tree'));
