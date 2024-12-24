602db0525f3016382b7f5e77949800111b337306
Oleksandra Kuziura <radioday2014@gmail.com>
2024-12-24 16:21:39 +0200

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,19 @@
 'use strict';
 
-// write code here
+const table = document.querySelector('.tree');
+
+table.onclick = function (e) {
+  const target = e.target;
+
+  if (target.tagName !== 'SPAN') {
+    return;
+  }
+
+  if (target.nextElementSibling.style.display === 'none') {
+    target.nextElementSibling.style.display = 'block';
+
+    return;
+  }
+
+  target.nextElementSibling.style.display = 'none';
+};
