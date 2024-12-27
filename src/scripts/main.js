917f07eebcc15c2581a1a80fa368245566d4caec
Nikita <onishenkonikitos@gmail.com>
2024-12-27 17:39:11 +0100

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,28 @@
 'use strict';
 
-// write code here
+const liElements = document.querySelectorAll('li');
+
+liElements.forEach((item) => {
+  if (item.childNodes.length > 0) {
+    const textNode = item.childNodes[0];
+    const textContent = textNode.textContent;
+
+    textNode.textContent = '';
+
+    const span = document.createElement('span');
+
+    span.textContent = textContent;
+
+    item.insertAdjacentElement('afterbegin', span);
+
+    span.addEventListener('click', (e) => {
+      e.stopPropagation();
+
+      const childrenContainer = item.querySelector('ul');
+
+      if (childrenContainer) {
+        childrenContainer.hidden = !childrenContainer.hidden;
+      }
+    });
+  }
+});
