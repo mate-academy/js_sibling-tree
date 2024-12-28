40ddc783cab913978e9eb33a19fde7a01679df6e
Zaransoftline <131307196+Zaransoftline@users.noreply.github.com>
2024-12-27 22:41:24 -0500

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,19 @@
 'use strict';
 
 // write code here
+
+const tree = document.querySelector('.tree');
+const lists = tree.querySelectorAll('ul');
+
+lists.forEach((list) => {
+  const closeBtn = list.previousElementSibling;
+
+  closeBtn.addEventListener('click', () => {
+    const toggleList = closeBtn.nextElementSibling;
+
+    if (toggleList) {
+      toggleList.style.display =
+        toggleList.style.display === 'none' ? 'block' : 'none';
+    }
+  });
+});
