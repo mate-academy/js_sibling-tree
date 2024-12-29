397095a2d89f003bf719b436bc70c44125565a4c
Anya Laban <laban.anya.a@gmail.com>
2024-12-29 18:06:53 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,20 @@
 'use strict';
 
-// write code here
+const treeItems = document.querySelectorAll('.tree li');
+
+treeItems.forEach((item) => {
+  const ul = item.querySelector('ul');
+
+  if (!ul) {
+    return;
+  }
+
+  const header = document.createElement('span');
+
+  header.textContent = item.firstChild.textContent.trim();
+  item.firstChild.replaceWith(header);
+
+  header.addEventListener('click', (e) => {
+    ul.style.display = ul.style.display === '' ? 'none' : '';
+  });
+});
