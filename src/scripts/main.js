19ee8520441b5e7dbc66b3b576f595e0e23915ef
Yehor <y.krapivka18@gmail.com>
2024-12-30 12:40:02 -0500

    final

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,32 @@
 'use strict';
 
-// write code here
+// отримуємо коллекцію усіх елементів списку
+const headline = document.querySelectorAll('li');
+
+function createSpan(el) {
+  const spanEl = document.createElement('span');
+  const textNode = el.firstChild;
+
+  el.firstChild.parentNode.insertBefore(spanEl, textNode);
+  spanEl.appendChild(textNode);
+}
+
+for (const el of headline) {
+  if (el.children.length > 0) {
+    createSpan(el);
+  }
+}
+
+const spanArray = [...document.querySelectorAll('span')];
+
+for (const li of spanArray) {
+  li.addEventListener('click', (e) => {
+    const children = li.nextElementSibling;
+
+    if (children.style.display === '') {
+      children.style.display = 'none';
+    } else {
+      children.style.display = '';
+    }
+  });
+}
