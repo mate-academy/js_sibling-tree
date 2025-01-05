77bc17937e0b577e33980b02ba9ee7bea935b22f
Pavlo Kotsan <paul@Pavlos-MacBook-Pro.local>
2025-01-05 10:51:48 -0500

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,27 @@
 'use strict';
 
-// write code here
+// Select headlines
+const headlines = document.querySelectorAll('.tree > li, .tree > li > ul > li');
+
+// Wrapping headlines into span tag
+headlines.forEach((headline) => {
+  const spanTag = document.createElement('span');
+
+  spanTag.textContent = headline.firstChild.textContent.trim();
+  headline.replaceChild(spanTag, headline.firstChild);
+});
+
+document.addEventListener('click', (e) => {
+  const clicked = e.target;
+
+  if (clicked.matches('.tree > li > span, .tree > li > ul > li > span')) {
+    const parentList = clicked.parentElement;
+    const childList = parentList.querySelector('ul');
+
+    if (childList) {
+      // Allow to expand and 'condense' list.
+      childList.style.display =
+        childList.style.display === 'none' ? 'block' : 'none';
+    }
+  }
+});
