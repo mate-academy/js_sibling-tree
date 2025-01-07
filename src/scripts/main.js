cea68de419de6b6dfbf205ef80b2b260a872bf85
Oleh Chernov <nineuito@mail.com>
2025-01-07 12:55:40 +0200

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,34 @@
 'use strict';
 
-// write code here
+const treeList = document.querySelector('.tree');
+
+function wrapperText() {
+  const items = treeList.querySelectorAll('li');
+
+  items.forEach((item) => {
+    const text = item.firstChild;
+
+    if (text.nodeType === 3) {
+      const span = document.createElement('span');
+
+      span.textContent = text.textContent;
+      text.replaceWith(span);
+    }
+  });
+}
+
+const isShowListTree = (e) => {
+  const target = e.target;
+
+  if (target.tagName === 'SPAN') {
+    const li = target.closest('li');
+    const ul = li.querySelector('ul');
+
+    if (ul) {
+      ul.hidden = !ul.hidden;
+    }
+  }
+};
+
+wrapperText();
+treeList.addEventListener('click', isShowListTree);
