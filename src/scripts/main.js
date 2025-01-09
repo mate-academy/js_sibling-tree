b308700e0fe23561d0e2d1b80830d0066c3b8c79
Ivan Solopenko <solopenko16@gmail.com>
2025-01-09 17:13:32 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,32 @@
 'use strict';
 
-// write code here
+const treeItems = document.querySelectorAll('.tree li');
+
+treeItems.forEach((item) => {
+  const firstTextNode = Array.from(item.childNodes).find(
+    (node) => node.nodeType === Node.TEXT_NODE,
+  );
+
+  if (firstTextNode) {
+    const spanTag = document.createElement('span');
+
+    spanTag.textContent = firstTextNode.textContent.trim();
+    item.replaceChild(spanTag, firstTextNode);
+  }
+
+  const span = item.querySelector('span');
+
+  if (span) {
+    span.addEventListener('click', () => {
+      const nestedList = item.querySelector('ul');
+
+      if (nestedList) {
+        if (nestedList.style.display === 'none') {
+          nestedList.style.display = 'block';
+        } else {
+          nestedList.style.display = 'none';
+        }
+      }
+    });
+  }
+});
