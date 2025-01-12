701e1a823335be6df36e3d86141ef97647b4b70a
Veronica Shyian <veronica.shyian@gmail.com>
2025-01-12 18:49:22 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,33 @@
 'use strict';
 
-// write code here
+const tree = document.querySelector('.tree');
+
+const listItems = document.querySelectorAll('li');
+
+const headlines = [...listItems].filter((item) => item.children.length);
+
+headlines.forEach((headline) => {
+  const spanElement = document.createElement('span');
+
+  spanElement.textContent = headline.firstChild.textContent;
+  headline.firstChild.textContent = '';
+  headline.insertAdjacentElement('afterbegin', spanElement);
+});
+
+tree.addEventListener('click', (ev) => {
+  if (ev.target.tagName.toLowerCase() === 'span') {
+    const li = ev.target.closest('li');
+
+    [...li.children].forEach((child, index) => {
+      if (index === 0) {
+        return;
+      }
+
+      if (!child.style.display) {
+        child.style.display = 'none';
+      } else {
+        child.removeAttribute('style');
+      }
+    });
+  }
+});
