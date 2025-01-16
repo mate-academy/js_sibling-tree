c408f74351a6690701bf5636541e645376c51c02
Oleksandr Poviakalo <oleksandr.poviakalo@gmail.com>
2025-01-16 12:56:56 +0200

    Task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,31 @@
 'use strict';
 
-// write code here
+function wrapTextWithSpan(element) {
+  for (const child of element.childNodes) {
+    if (child.nodeType === Node.TEXT_NODE && child.textContent.trim() !== '') {
+      const span = document.createElement('span');
+
+      span.textContent = child.textContent.trim();
+
+      element.replaceChild(span, child);
+    } else if (child.nodeType === Node.ELEMENT_NODE) {
+      wrapTextWithSpan(child);
+    }
+  }
+}
+
+const tree = document.querySelector('.tree');
+
+wrapTextWithSpan(tree);
+
+document.addEventListener('click', (e) => {
+  const hideBlock = e.target.nextSibling;
+
+  if (hideBlock) {
+    if (hideBlock.style.display === 'none') {
+      hideBlock.style.display = 'block';
+    } else {
+      hideBlock.style.display = 'none';
+    }
+  }
+});
