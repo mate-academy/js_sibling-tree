f318793dc7ef69fee3ed1a9c89160ff1d927a048
Denys <zorosmg5@mail.com>
2025-01-17 15:12:31 +0200

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,17 @@
 'use strict';
 
-// write code here
+document.addEventListener('DOMContentLoaded', function () {
+  const headers = document.querySelectorAll('li > span');
+
+  headers.forEach((header) => {
+    header.addEventListener('click', function (e) {
+      e.stopPropagation();
+
+      const childList = this.parentElement.querySelector('ul');
+
+      if (childList) {
+        childList.classList.toggle('hidden');
+      }
+    });
+  });
+});
