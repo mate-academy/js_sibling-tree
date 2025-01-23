06d2b3eb75482becbca535343e78f29f02e01ddd
Oleg Orykhivskiy <olegkoolll@gmail.com>
2025-01-23 16:12:46 +0200

    done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,26 @@
 'use strict';
 
-// write code here
+const ul = document.querySelector('.tree');
+
+// eslint-disable-next-line no-shadow
+ul.addEventListener('click', function (event) {
+  const li = event.target.closest('li');
+
+  if (li) {
+    const inerUL = li.querySelector('ul');
+
+    if (li.style.fontWeight) {
+      li.style.fontWeight = null;
+    } else {
+      li.style.fontWeight = 'bold';
+    }
+
+    if (inerUL) {
+      if (inerUL.style.display === 'none') {
+        inerUL.style.display = 'block';
+      } else {
+        inerUL.style.display = 'none';
+      }
+    }
+  }
+});
