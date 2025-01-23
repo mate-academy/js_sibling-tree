e50e3aacaee32c5e2db0090439d29a116bdb744b
Anton Tur <ant.tur85@gmail.com>
2025-01-23 15:10:23 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,34 @@
 'use strict';
 
-// write code here
+const list = document.querySelectorAll('li');
+
+function addSpanToList(listElement) {
+  listElement.forEach((li) => {
+    const span = document.createElement('span');
+
+    span.textContent = li.firstChild.textContent;
+
+    if (span.textContent[0] === '\n') {
+      span.classList.add('title');
+      span.style.display = 'block';
+    }
+
+    li.replaceChild(span, li.firstChild);
+  });
+}
+
+addSpanToList(list);
+
+const titleList = document.querySelectorAll('.title');
+
+titleList.forEach((item) => {
+  item.addEventListener('click', () => {
+    const ul = item.nextElementSibling;
+
+    if (ul.style.display === 'none') {
+      ul.style.display = 'block';
+    } else {
+      ul.style.display = 'none';
+    }
+  });
+});
