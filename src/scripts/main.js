15500b66ab1ce4a2794bca857ecce1a073f48461
Nikolay <diedaemon@gmail.com>
2025-01-25 18:25:19 +0100

    task complete

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,24 @@
 'use strict';
 
-// write code here
+const tree = document.querySelector('.tree');
+const liItems = tree.querySelectorAll('li:has(> ul)');
+
+liItems.forEach((item) => {
+  const span = document.createElement('span');
+  const textContent = item.firstChild.textContent.trim();
+
+  span.textContent = textContent;
+  span.style.display = 'block';
+  span.classList.add('span-class');
+  item.replaceChild(span, item.firstChild);
+});
+
+const spanItems = document.querySelectorAll('.span-class');
+
+spanItems.forEach((item) => {
+  item.addEventListener('click', () => {
+    const elem = item.nextElementSibling;
+
+    elem.style.display = elem.style.display === '' ? 'none' : '';
+  });
+});
