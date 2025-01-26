3d06ff0a7fdd323efcbdbf48934c178f9b709fb6
Ihor Barban <ihorbarban@Ihors-MacBook-Air.local>
2025-01-26 21:34:46 +0100

    Not my solution, im exhausted

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,31 @@
-'use strict';
+const list = document.querySelector('.tree');
 
-// write code here
+function getSlidingTree(tree) {
+  const listItems = tree.children;
+
+  [...listItems].forEach((item) => {
+    const nestedList = item.children[0];
+
+    if (nestedList) {
+      const headlineWrapper = document.createElement('span');
+
+      headlineWrapper.className = 'headline';
+
+      headlineWrapper.append(item.childNodes[0]);
+
+      headlineWrapper.addEventListener('click', () => {
+        if (nestedList.style.display === 'none') {
+          nestedList.style.display = 'block';
+        } else {
+          nestedList.style.display = 'none';
+        }
+      });
+
+      item.prepend(headlineWrapper);
+
+      getSlidingTree(nestedList);
+    }
+  });
+}
+
+getSlidingTree(list);
