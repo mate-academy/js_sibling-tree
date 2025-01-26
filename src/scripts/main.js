b578952c0d1430b3f0af05197ce5143f946cbf6d
Albina <yakunina.albina@gmail.com>
2025-01-26 14:46:52 +0100

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,21 @@
 'use strict';
 
-// write code here
+const liItems = document.querySelectorAll('li > ul');
+
+liItems.forEach((el) => {
+  const parentLi = el.parentElement;
+
+  const text = parentLi.firstChild.textContent.trim();
+
+  const span = document.createElement('span');
+
+  span.textContent = text;
+
+  parentLi.firstChild.replaceWith(span);
+
+  el.style.display = 'block';
+
+  span.addEventListener('click', () => {
+    el.style.display = el.style.display === 'block' ? 'none' : 'block';
+  });
+});
