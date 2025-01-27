8ff69b4485116666e52c6cdfcd0a75c2baf70333
Bilokon Yaroslav <uvill.github@gmail.com>
2025-01-27 15:55:43 +0200

    make sliding tree DOM

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,33 @@
 'use strict';
 
-// write code here
+const list = document.querySelector('.tree');
+const listItem = [...list.querySelectorAll('li')];
+
+for (const item of listItem) {
+  if (item.querySelector('ul') !== null) {
+    const span = document.createElement('span');
+
+    span.textContent = item.textContent.trim().split(' ')[0];
+    item.prepend(span);
+    item.childNodes[1].textContent = null;
+  }
+}
+
+const listSpan = [...list.querySelectorAll('span')];
+
+// console.log(listSpan);
+
+for (const span of listSpan) {
+  span.addEventListener('click', (e) => {
+    const nextUl = e.currentTarget.nextElementSibling;
+
+    if (
+      nextUl.getAttribute('style') === 'display: auto' ||
+      nextUl.getAttribute('style') === null
+    ) {
+      nextUl.setAttribute('style', 'display: none;');
+    } else {
+      nextUl.setAttribute('style', 'display: auto');
+    }
+  });
+}
