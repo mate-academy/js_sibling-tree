61c77dd8b8086c43ec3049cab776179c74f799ea
Oleh Mysko <olegmysko1996@gmail.com>
2025-01-27 14:24:46 +0200

    task

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,28 @@
 'use strict';
 
-// write code here
+const list = document.querySelector('.tree');
+const li = list.querySelectorAll('li');
+
+li.forEach((element) => {
+  const childNodes = Array.from(element.childNodes);
+  const textnode = childNodes.find((node) => node.nodeType === 3);
+
+  if (textnode) {
+    const span = document.createElement('span');
+
+    span.textContent = textnode.nodeValue;
+    element.replaceChild(span, textnode);
+
+    span.addEventListener('click', (eve) => {
+      const childUl = element.querySelector('ul');
+
+      if (childUl) {
+        if (childUl.style.display === 'none') {
+          childUl.style.display = 'block';
+        } else {
+          childUl.style.display = 'none';
+        }
+      }
+    });
+  }
+});
