153e1eca6d249b07bd3f4ad8fb387bdc4bd2a5f6
Maksym <makstarasovv1@gmail.com>
2025-01-28 20:08:14 +0200

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,29 @@
 'use strict';
 
-// write code here
+const items = document.querySelectorAll('li');
+
+items.forEach((item) => {
+  const header = item.firstChild;
+
+  if (header && header.nodeType === 3) {
+    header.addEventListener('click', function (e) {
+      e.stopPropagation();
+
+      const nestedList = item.querySelector('ul');
+
+      if (nestedList) {
+        nestedList.classList.toggle('hidden');
+      }
+    });
+  }
+});
+
+document.addEventListener('click', function (e) {
+  const nestedLists = document.querySelectorAll('ul');
+
+  nestedLists.forEach((list) => {
+    if (!list.contains(e.target)) {
+      list.classList.add('hidden');
+    }
+  });
+});
