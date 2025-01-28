1922a3616999f664009307554c2843940bd6f377
Hanna Balabukha <anna.aniskovets1992@ukr.net>
2025-01-28 13:50:41 +0200

    develop

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,33 @@
 'use strict';
 
-// write code here
+const tree = document.querySelector('.tree');
+const liItems = tree.querySelectorAll('li:has(> ul)');
+
+tree.addEventListener('click', (event) => {
+
+  let tar = event.target;
+
+  if(!tar.matches('li:has(ul)')) {
+    if(tar.matches('li')) {
+      return;
+    }
+   tar = tar.closest('li')
+
+   if(tar == null || !tar.matches('li:has(ul)')) {
+    return;
+   }
+  }
+
+  if (tar == null) {
+    return;
+  }
+
+  const items = tar.querySelector('ul');
+
+  if (items.style.display === 'none') {
+    items.style.display = 'block';
+    }
+    else {
+      items.style.display = 'none';
+    }
+});
