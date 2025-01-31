d03d8f61c9d55bcf12c1b1fd593a2f25bf463e5e
marushchak <marushchak099@gmail.com>
2025-01-31 19:08:55 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,25 @@
 'use strict';
 
-// write code here
+const tree = document.querySelector('.tree');
+const li = document.querySelectorAll('.tree li');
+
+li.forEach((el) => {
+  const textNode = [...el.childNodes].find((node) => {
+    return node.nodeType === Node.TEXT_NODE && node.textContent.trim();
+  });
+
+  if (textNode && el.querySelector('ul')) {
+    const spanElement = document.createElement('span');
+
+    spanElement.textContent = textNode.textContent.trim();
+    el.replaceChild(spanElement, textNode);
+  }
+});
+
+tree.addEventListener('click', (e) => {
+  const span = e.target.closest('span');
+  const elAfterSpan = span.nextSibling;
+
+  elAfterSpan.style.display =
+    elAfterSpan.style.display === 'none' ? 'block' : 'none';
+});
