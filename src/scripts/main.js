c98110d507debad21454da242d9b025cc670ca6c
Mykhailo <kurochkinmy@gmail.com>
2025-02-03 21:52:50 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,6 +4,23 @@ const tree = document.querySelector('.tree');
 const listOfFirstLevelHeaders = [];
 const listOfSecondLevelHeaders = [];
 
+
+
+for (let i = 0; i < tree.children.length; i++) {
+  for (let j = 0; j < tree.children[i].children[0].children.length; j++) {
+    const span = document.createElement('span');
+
+    span.textContent =
+      tree.children[i].children[0].children[j].firstChild.textContent;
+    tree.children[i].children[0].children[j].firstChild.textContent = '';
+    tree.children[i].children[0].children[j].prepend(span);
+
+    listOfSecondLevelHeaders.push(
+      tree.children[i].children[0].children[j].firstChild,
+    );
+  }
+}
+
 for (let i = 0; i < tree.children.length; i++) {
   const span = document.createElement('span');
 
@@ -18,22 +35,9 @@ for (let i = 0; i < tree.children.length; i++) {
   listOfFirstLevelHeaders.push(tree.children[i].firstChild);
 }
 
-for (let i = 0; i < tree.children.length; i++) {
-  for (let j = 0; j < tree.children[i].children[0].children.length; j++) {
-    const span = document.createElement('span');
-
-    span.textContent =
-      tree.children[i].children[0].children[j].firstChild.textContent;
-    tree.children[i].children[0].children[j].firstChild.textContent = '';
-
-    span.addEventListener('click', (e) => {
-      span.parentElement.children[1].hidden =
-        !span.parentElement.children[1].hidden;
-    });
-    tree.children[i].children[0].children[j].prepend(span);
-
-    listOfSecondLevelHeaders.push(
-      tree.children[i].children[0].children[j].firstChild,
-    );
-  }
+for (const item of listOfSecondLevelHeaders) {
+  item.addEventListener('click', (e) => {
+    item.parentElement.children[1].hidden =
+      !item.parentElement.children[1].hidden;
+  });
 }
