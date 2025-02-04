a203fb6e6228d39707642565cb011012b6d9015f
Uusuff <90629840+Uusuff@users.noreply.github.com>
2025-02-04 17:39:59 +0100

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,31 @@
 'use strict';
 
-// write code here
+const tree = document.querySelector('.tree');
+
+tree.querySelectorAll('li').forEach((li) => {
+  const text = li.firstChild.textContent.trim();
+
+  if (!text) {
+    return;
+  }
+
+  const span = document.createElement('span');
+
+  span.textContent = text;
+  li.firstChild.replaceWith(span);
+});
+
+tree.addEventListener('click', (e) => {
+  const title = e.target.closest('span');
+
+  if (!title) {
+    return;
+  }
+
+  const parentLi = title.parentElement;
+  const sublist = parentLi.querySelector('ul');
+
+  if (sublist) {
+    sublist.hidden = !sublist.hidden;
+  }
+});
