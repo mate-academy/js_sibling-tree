e2a830bdb50bb4e662f00ca79d54ac62fe6e75dc
denys <denis.bielykh38@gmail.com>
2025-02-05 22:13:29 +0200

    solution-1

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,34 @@
 'use strict';
 
-// write code here
+const tree = document.querySelector('.tree');
+
+createSpanForHeaders(tree);
+
+function createSpanForHeaders(list) {
+  const listsInTree = tree.querySelectorAll('ul');
+
+  for (const ul of listsInTree) {
+    const headerNode = ul.parentNode.firstChild;
+    const headerText = headerNode.textContent;
+
+    const span = document.createElement('span');
+    span.innerText = headerText;
+
+    headerNode.replaceWith(span);
+  }
+}
+
+tree.addEventListener('click', (event) => {
+  const target = event.target;
+
+  if (target.nodeName !== 'SPAN') return;
+
+  const ul = target.nextElementSibling;
+
+  if (ul.style.display === 'none') {
+    ul.style.display = 'block';
+  } else {
+    ul.style.display = 'none';
+  }
+})
+
