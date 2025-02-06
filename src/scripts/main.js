f0c7d2edbf7c4d855883339b4b64b02aa52ffb9a
Nataliia Nudyk <nataliianudyk@gmail.com>
2025-02-06 18:06:15 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,37 @@
 'use strict';
 
-// write code here
+const tree = document.querySelector('.tree');
+
+function addSpan(list) {
+  for (const el of list.querySelectorAll('li')) {
+    const content = el.firstChild.textContent;
+
+    const span = document.createElement('span');
+
+    span.textContent = content;
+
+    el.firstChild.replaceWith(span);
+  }
+}
+
+addSpan(tree);
+
+tree.addEventListener('click', function (e) {
+  if (e.target.closest('li')) {
+    const article = e.target.closest('li');
+    const ul = article.querySelector('ul');
+    let articelComputed;
+
+    if (ul) {
+      articelComputed = getComputedStyle(ul);
+    } else {
+      return;
+    }
+
+    if (articelComputed.getPropertyValue('display') === 'block') {
+      article.querySelector('ul').style.display = 'none';
+    } else {
+      article.querySelector('ul').style.display = 'block';
+    }
+  }
+});
