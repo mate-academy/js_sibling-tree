d3e67dae9e4915f7dd778c92f205ecdb4d145f2e
Vladyslav Kutsyn <vladislavkucn@gmail.com>
2025-02-07 14:24:08 +0100

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,21 @@
 'use strict';
 
-// write code here
+document.addEventListener('DOMContentLoaded', () => {
+  document.querySelectorAll('.tree li').forEach((li) => {
+    if (li.querySelector('ul')) {
+      li.style.cursor = 'pointer';
+
+      li.addEventListener('click', (e) => {
+        e.stopPropagation();
+
+        const sublist = li.querySelector('ul');
+
+        if (sublist) {
+          sublist.style.display =
+            sublist.style.display === 'none' ? 'block' : 'none';
+        }
+      });
+      li.querySelector('ul').style.display = 'none';
+    }
+  });
+});
