9a2adc34e14b52451821c064ab4591932d9967d5
iteamfox <avlisunenko@gmail.com>
2025-02-10 21:01:49 +0100

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,25 @@
 'use strict';
 
 // write code here
+document.addEventListener('DOMContentLoaded', () => {
+  const treeItems = document.querySelectorAll('li');
+
+  treeItems.forEach((item) => {
+    const nestedList = item.querySelector('ul');
+
+    if (nestedList) {
+      nestedList.style.display = 'none';
+
+      item.addEventListener('click', (e) => {
+        e.stopPropagation();
+
+        const currentNestedList = item.querySelector('ul');
+
+        if (currentNestedList) {
+          currentNestedList.style.display =
+            currentNestedList.style.display === 'none' ? 'block' : 'none';
+        }
+      });
+    }
+  });
+});
