46046a3ca7b89e08138529f7a64829e94bf403ed
VadimKudrenko <48049362+VadimKudrenko@users.noreply.github.com>
2025-02-11 11:52:48 +0200

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,18 @@
 'use strict';
 
-// write code here
+document.querySelectorAll('li').forEach((li) => {
+  const span = document.createElement('span');
+
+  span.textContent = li.firstChild.textContent.trim();
+
+  li.firstChild.textContent = '';
+  li.prepend(span);
+});
+
+document.querySelectorAll('span').forEach((span) => {
+  span.addEventListener('click', () => {
+    const ulTopic = span.parentElement.querySelector('ul');
+
+    ulTopic.style.display = ulTopic.style.display === 'none' ? '' : 'none';
+  });
+});
