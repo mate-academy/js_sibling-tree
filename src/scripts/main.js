5ce30a5883089efaaa9c2bf9f5d3feb28f18c065
Vladyslav Kutsyn <vladislavkucn@gmail.com>
2025-02-12 14:28:53 +0100

    add soltuion

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,20 @@
 'use strict';
 
-// write code here
+document.querySelectorAll('.tree li').forEach((li) => {
+  const span = document.createElement('span');
+
+  span.textContent = li.childNodes[0].nodeValue.trim();
+  li.childNodes[0].replaceWith(span);
+
+  span.addEventListener('click', () => {
+    const childUl = li.querySelector('ul');
+
+    if (childUl) {
+      if (childUl.style.display === 'none') {
+        childUl.style.display = 'block';
+      } else {
+        childUl.style.display = 'none';
+      }
+    }
+  });
+});
