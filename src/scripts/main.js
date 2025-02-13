f842696ace527036aafec735f684ccad605b1542
Alla Serhiienko <allasergienko706@gmail.com>
2025-02-13 21:51:01 +0100

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,19 @@
 'use strict';
 
-// write code here
+const headers = document.querySelectorAll('li:has(ul)');
+
+headers.forEach((header) => {
+  const span = document.createElement('span');
+
+  span.textContent = header.firstChild.textContent;
+  header.firstChild.textContent = '';
+  header.prepend(span);
+
+  const ul = header.querySelector('ul');
+
+  span.addEventListener('click', (e) => {
+    if (ul) {
+      ul.style.display = ul.style.display === 'none' ? 'block' : 'none';
+    }
+  });
+});
