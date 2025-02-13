f77da9d1c5dfca99554dad3af0bb0d22d4386cfb
Alina Isakova <adorosh062@gmail.com>
2025-02-13 13:26:16 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,24 @@
 'use strict';
 
-// write code here
+const tree = document.querySelector('.tree');
+
+const list = tree.querySelectorAll('li');
+
+for (const li of list) {
+  if (li.querySelector('ul')) {
+    const span = document.createElement('span');
+
+    span.textContent = li.firstChild.textContent.trim();
+    li.firstChild.replaceWith(span);
+  }
+}
+
+const spanList = document.querySelectorAll('span');
+
+for (const span of spanList) {
+  span.onclick = function () {
+    const subList = span.nextElementSibling;
+
+    subList.style.display = subList.style.display === 'none' ? 'block' : 'none';
+  };
+}
