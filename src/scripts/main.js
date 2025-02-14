4c4808a9411f01dab04507fb07defb0243097ab9
Maksym Kostetskyi <mkost@gmail.com>
2025-02-14 11:03:32 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,29 @@
 'use strict';
 
 // write code here
+const lis = document.querySelectorAll('.tree li');
+
+lis.forEach((li) => {
+  const text = li.firstChild;
+  const newSpan = document.createElement('span');
+
+  newSpan.className = 'textWrapper';
+  newSpan.appendChild(document.createTextNode(text.nodeValue));
+  li.replaceChild(newSpan, text);
+});
+
+const textWrappers = document.querySelectorAll('.textWrapper');
+
+textWrappers.forEach((textWrapper) => {
+  textWrapper.addEventListener('click', (e) => {
+    const nextElement = e.target.nextSibling;
+
+    if (nextElement.style.display === 'none') {
+      nextElement.style.display = 'block';
+
+      return;
+    }
+
+    nextElement.style.display = 'none';
+  });
+});
