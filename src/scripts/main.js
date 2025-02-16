8302de5ddbe65ee22617224a88c6e2f7c7bb5209
Oleh <pryge321@gmail.com>
2025-02-16 20:03:47 +0200

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,21 @@
 'use strict';
 
-// write code here
+const tree = document.querySelector('.tree');
+
+tree.addEventListener('click', (e) => {
+  const span = e.target.closest('span');
+
+  if (!span) {
+    return;
+  }
+
+  const ul = span.nextElementSibling;
+
+  if (ul.style.display !== 'none') {
+    ul.style.display = 'none';
+
+    return;
+  }
+
+  ul.style.display = 'block';
+});
