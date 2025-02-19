3002adcaa884fcec6af7efa6124190bf0809c16a
Kyrylo Bondarenko <dejsk35@gmail.com>
2025-02-19 11:02:06 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,14 @@
 'use strict';
 
-// write code here
+const ulElement = document.querySelector('.tree');
+
+ulElement.addEventListener('click', (e) => {
+  const sublingItem = e.target.nextElementSibling;
+
+  if (!sublingItem) {
+    return;
+  }
+
+  sublingItem.style.display =
+    sublingItem.style.display === 'none' ? 'block' : 'none';
+});
