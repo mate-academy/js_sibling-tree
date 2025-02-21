13b442bdebeff5f7eb344c1c11b57fcde20c3f49
Vladyslav Tsarenko <vaaaltaaasaaar@gmail.com>
2025-02-21 08:33:46 -0600

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,17 @@
 'use strict';
 
-// write code here
+const deepListElem = [...document.querySelectorAll('li > ul')];
+
+deepListElem.forEach((el) => {
+  const textNode = el.parentElement.firstChild;
+  const spanEl = document.createElement('span');
+
+  spanEl.textContent = textNode.textContent;
+  textNode.replaceWith(spanEl);
+});
+
+document.addEventListener('click', (e) => {
+  if (e.target.closest('span')) {
+    e.target.nextElementSibling.hidden = !e.target.nextElementSibling.hidden;
+  }
+});
