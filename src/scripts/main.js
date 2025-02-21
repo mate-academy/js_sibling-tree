0c34804c729ebeee87959f650ff79d5773036760
Vitalii Kredinets <kredinec.v@gmail.com>
2025-02-21 14:15:06 +0200

    solve task

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,34 @@
 'use strict';
 
 // write code here
+
+document.addEventListener('DOMContentLoaded', () => {
+  const tree = document.querySelector('.tree');
+
+  const list = tree.querySelectorAll('li');
+
+  list.forEach((li) => {
+    const childNodes = Array.from(li.childNodes);
+
+    childNodes.forEach((node) => {
+      if (node.nodeType === 3) {
+        const span = document.createElement('span');
+
+        span.textContent = node.textContent;
+        node.parentNode.replaceChild(span, node);
+      }
+    });
+  });
+
+  tree.addEventListener('click', (e) => {
+    if (e.target.tagName === 'SPAN') {
+      const parentLi = e.target.parentElement;
+      const sublist = parentLi.querySelector('ul');
+
+      if (sublist) {
+        sublist.style.display =
+          sublist.style.display === 'none' ? 'block' : 'none';
+      }
+    }
+  });
+});
