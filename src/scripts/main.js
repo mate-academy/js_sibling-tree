cfc0dff191254c3cf7039a9d9dc0329a3970eb2d
Anna Poplavska <annapoplavska10@gmail.com>
2025-02-27 11:02:17 +0100

    Task done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,16 @@
 'use strict';
 
-// write code here
+document.querySelectorAll('.tree li').forEach((li) => {
+  if (li.children.length && li.firstChild.nodeType === Node.TEXT_NODE) {
+    const span = document.createElement('span');
+
+    span.textContent = li.firstChild.textContent.trim();
+    li.firstChild.replaceWith(span);
+
+    span.addEventListener('click', (e) => {
+      const ul = e.target.nextElementSibling;
+
+      ul.style.display = ul.style.display === 'none' ? 'block' : 'none';
+    });
+  }
+});
